ff427c8013c1e8b4a9a81f242d6565cc46bcb1b9
ahmedhabeeb73 <alnaib888@gmail.com>
2021-08-28 18:08:01 +0200

    add blogs

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
+import {useState} from 'react';
+
 const Home = () => {
+  
+  const [blogs,setBlogs]=useState([
+    {title:"title1",body:"body1",author:"ahmed",id:1},
+    {title:"title2",body:"body1",author:"dfdf",id:2},
+    {title:"title3",body:"body1",author:"ali",id:3},
+  ]);
+
   return (  
     <div className="home">
-      <h2>Home</h2>
+     {blogs.map((blog)=>(
+       <div className="blog-preview" key={blog.id}>
+         <h2>{blog.title}</h2>
+         <p>written by:{blog.author}</p>
+      </div>
+     ))}
     </div>
   );
 }
